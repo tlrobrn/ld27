1628fc6ef83def2d432d980d947565c2d7d79c22
Taylor O'Brien <t08rien@gmail.com>
2013-08-25 14:20:38 -0500

    new level and tileset

--- src/js/config.js
+++ src/js/config.js
@@ -1,14 +1,14 @@
 'use strict';
 var game = {
-    width: 640,
+    width: 1280,
     height: 480,
     tile_width: 32,
     tile_height: 32,
     map_width: 40,
-    map_height: 30,
+    map_height: 15,
     start: function () {
         Crafty.init(game.width, game.height);
-        Crafty.viewport.init(game.width/2, game.height)
+        Crafty.viewport.init(game.width/4, game.height)
         Crafty.scene('Loading');
     }
 };
